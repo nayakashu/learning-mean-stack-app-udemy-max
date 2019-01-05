1bb73bfb4c0759296af80a6cd0aa52de58bc7fa0
Ashutosh Nayak <Ashutosh.Nayak@sc.com>
2019-01-06 00:26:28 +0530

    [Express] - Updated express app to allow specific headers and methods from the client

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,14 @@ const express = require('express');
 
 const app = express();
 
+// To allow CORS
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
+  next();
+});
+
 app.use('/api/posts', (req, res, next) => {
   const posts = [
     { id: 'tugcsi123',
