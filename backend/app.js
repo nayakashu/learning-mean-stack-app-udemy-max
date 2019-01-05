fd60996a819cb4b19d1ab47e8a20c65163e6c5ef
Ashutosh Nayak <Ashutosh.Nayak@sc.com>
2019-01-06 00:37:14 +0530

    [Express] - Added body-parser and post endpoint to express app

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // To allow CORS
 app.use((req, res, next) => {
@@ -8,6 +12,14 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
   next();
+});
+
+app.post('/api/posts', (req, res) => {
+  const post = req.body;
+  console.log(post);
+  res.status(201).json({
+    message: 'Post added successfully'
+  });
 });
 
 app.use('/api/posts', (req, res, next) => {
