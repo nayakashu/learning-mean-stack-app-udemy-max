e44054bc1e9e84f78845a293fb6c3ed5431beabe
Ashutosh Nayak <Ashutosh.Nayak@sc.com>
2019-01-05 21:28:06 +0530

    [Express] - Added first route '/api/posts' to express

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,22 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('First middleware');
-  next();
-});
+app.use('/api/posts', (req, res, next) => {
+  const posts = [
+    { id: 'tugcsi123',
+      title: 'First server-side post',
+      content: 'This is coming from the server'
+    },
+    { id: 'skhaga123',
+      title: 'Second server-side post',
+      content: 'This is coming from the server too!'
+    }
+  ];
 
-app.use((req, res, next) => {
-  res.send('Hello from express!');
+  res.status(200).json({
+    message: 'Posts fetched successfully!',
+    posts: posts
+  });
 });
 
 module.exports = app;
