d1fbf34a76e5bf7b6affd58d0643de1526965941
Ashutosh Nayak <Ashutosh.Nayak@sc.com>
2019-01-06 15:21:42 +0530

    [Express] - Fetching posts from Mongodb during page load

--- backend/app.js
+++ backend/app.js
@@ -37,21 +37,13 @@ app.post('/api/posts', (req, res) => {
 });
 
 app.get('/api/posts', (req, res) => {
-  const posts = [
-    { id: 'tugcsi123',
-      title: 'First server-side post',
-      content: 'This is coming from the server'
-    },
-    { id: 'skhaga123',
-      title: 'Second server-side post',
-      content: 'This is coming from the server too!'
-    }
-  ];
-
-  res.status(200).json({
-    message: 'Posts fetched successfully!',
-    posts: posts
-  });
+  Post.find()
+    .then(documents => {
+      res.status(200).json({
+        message: 'Posts fetched successfully!',
+        posts: documents
+      });
+    });
 });
 
 module.exports = app;
